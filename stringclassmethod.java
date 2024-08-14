024840570f718a4004eb79dfb84b5980c6475e26
surendran <162984948+surendran-07@users.noreply.github.com>
2024-08-14 11:23:43 +0530

    Add files via upload

---
+++ stringclassmethod.java
@@ -0,0 +1,21 @@
+package surendran01;
+
+public class prg4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="hello world";
+int lenght=str.length();
+char firstchar=str.charAt(0);
+String uppercase=str.toUpperCase();
+String lowercase=str.toLowerCase();
+String replace=str.replace("hello","hi");
+System.out.println("orignal string"+str);
+System.out.println("first character"+firstchar);
+System.out.println("upper case"+uppercase);
+System.out.println("lowercase"+lowercase);
+System.out.println("replaced string"+replace);
+
+	}
+
+}
